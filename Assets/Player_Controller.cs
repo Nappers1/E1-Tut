fa5984a29bdec95a5d52975307423c3e03044656
ShiningNIGHTZ <gangyichen.gc@gmail.com>
2026-01-15 17:40:48 -0800

    Collectibles

--- Assets/Player_Controller.cs
+++ Assets/Player_Controller.cs
@@ -7,17 +7,25 @@ public class Player_Controller : MonoBehaviour
     float movementX;
     float movementY;
     [SerializeField] float speed = 5.0f;
+    Rigidbody2D rb;
+    bool isGrounded = true;
+    int score = 0;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        float movementDistanceX = movementX * speed * Time.deltaTime;
-        float movementDistanceY = movementY * speed * Time.deltaTime;
-        transform.position = new Vector2(transform.position.x + movementDistanceX, transform.position.y + movementDistanceY);
+        //float movementDistanceX = movementX * speed * Time.deltaTime;
+        //float movementDistanceY = movementY * speed * Time.deltaTime;
+        //transform.position = new Vector2(transform.position.x + movementDistanceX, transform.position.y + movementDistanceY);
+        rb.linearVelocity = new Vector2(movementX * speed, rb.linearVelocity.y);
+        if (isGrounded && movementY > 0)
+        {
+            rb.AddForce(new Vector2(0, 100));
+        }
     }
 
     void OnMove(InputValue value)
@@ -25,7 +33,33 @@ public class Player_Controller : MonoBehaviour
         Vector2 v = value.Get<Vector2>();
         movementX = v.x;
         movementY = v.y;
-        Debug.Log("Movement X = " + movementX);
-        Debug.Log("Movement Y = " + movementY);
+        //Debug.Log("Movement X = " + movementX);
+        //Debug.Log("Movement Y = " + movementY);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Collectible"))
+        {
+            score++;
+            collision.gameObject.SetActive(false);
+            Debug.Log("Score: " + score);
+        }
     }
 }
